f9822df8cb3c5f684453a7464083531677ed62e1
m4dr1nch <martins.sav@protonmail.com>
2022-09-08 12:18:07 +0300

    Version 1.1 (Color update)

--- pyrolog.py
+++ pyrolog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -16,38 +16,14 @@ __author__ = "m4dr1nch"
 __date__ = "2022/09/05"
 __deprecated__ = False
 __license__ = "GPLv3"
-__version__ = "1.0"
+__version__ = "1.1"
 
 import sys
 import os
 import shutil
 
-def banner():
-    print("")
-    print("               (&&      @&(")
-    print("              %% &     &. /&")
-    print("              &.  @#& %#    @&")
-    print("              \&     %&*      %&")
-    print("          .__   &      $/       &%")
-    print("         %&  (&@%..              @*    .%")
-    print("           &                     &(    .&&*")
-    print("           #&\  ▄▄▄· ▄· ▄▌▄▄▄     @,   %%  %%")
-    print("  &#\       &  ▐█ ▄█▐█▪██▌▀▄ █·▪   \(*/&     &,")
-    print("./&  &    %/*   ██▀·▐█▌▐█▪▐▀▀▄  ▄█▀▄        %&")
-    print(" %&   \$$      ▐█▪·• ▐█▀·.▐█•█▌▐█▌.▐▌       &")
-    print("  *@#          .▀     ▀ • .▀  ▀ ▀█▄▀▪     ,&")
-    print("    @&                                  &@")
-    print("  .<!%#$%#$$$%#$#@$#%#@#$#%#$%#$@#$@%@###$%^&>.")
-    print("/@(        ====            ========    &(     &\\")
-    print("@/                 ╦  ╔═╗╔═╗          &,  \\\\\\  &\\")
-    print("&%            ───  ║  ║ ║║ ╦  ───     &  \\\\\\\\\\ #&")
-    print("\&.   =====        ╩═╝╚═╝╚═╝          #%  \\\\\\  &/")
-    print(" \&..      ====          =========     *&,   %&/")
-    print("   *<%#$%#$$&^%*%$#$$%#$^$^&$#$$@#%$%&%^%$^&>*")
-    print("      Created by: " + __author__)
-    print("      Version: " + __version__)
-    print("")
-
+# This is by no means a comprehensive list.
+# Suggestions are encouraged!
 logFiles = [
     "/var/log/lastlog",
     "/var/log/messages",
@@ -138,17 +114,96 @@ logFileDirs = [
     "/var/log/landscape/"
 ]
 
+def banner():
+    print(f'{colors.BOLD}{colors.B_RED}')
+    print(f'               (&&      @&(')
+    print(f'              %% &     &. /&')
+    print(f'              &.  @#& %#    @&')
+    print(f'              \&     %&*      %&')
+    print(f'          .__   &      $/       &%')
+    print(f'         %&  (&@%..              @*    .%')
+    print(f'           &                     &(    .&&*')
+    print(f'           #&\  {colors.B_YELLOW}▄▄▄· ▄· ▄▌▄▄▄{colors.B_RED}     @,   %%  %%')
+    print(f'  &#\       &  {colors.B_YELLOW}▐█ ▄█▐█▪██▌▀▄ █·▪{colors.B_RED}   \(*/&     &,')
+    print(f'./&  &    %/*   {colors.B_YELLOW}██▀·▐█▌▐█▪▐▀▀▄  ▄█▀▄{colors.B_RED}        %&')
+    print(f' %&   \$$      {colors.B_YELLOW}▐█▪·• ▐█▀·.▐█•█▌▐█▌.▐▌{colors.B_RED}       &')
+    print(f'  *@#          {colors.B_YELLOW}.▀     ▀ • .▀  ▀ ▀█▄▀▪{colors.B_RED}     ,&')
+    print(f'    @&                                  &@')
+    print(f'  {colors.BROWN}.<!%#$%#$$$%#$#@$#%#@#$#%#$%#$@#$@%@###$%^&>.')
+    print(f'/@(        ====            ========    &(     &\\')
+    print(f'@/                 {colors.B_YELLOW}╦  ╔═╗╔═╗{colors.BROWN}          &,  {colors.B_YELLOW}\\\\\\{colors.BROWN}  &\\')
+    print(f'&%            ───  {colors.B_YELLOW}║  ║ ║║ ╦{colors.BROWN}  ───     &  {colors.B_YELLOW}\\\\\\\\\\{colors.BROWN} #&')
+    print(f'\&.   =====        {colors.B_YELLOW}╩═╝╚═╝╚═╝{colors.BROWN}          #%  {colors.B_YELLOW}\\\\\\{colors.BROWN}  &/')
+    print(f' \&..      ====          =========     *&,   %&/')
+    print(f'   *<%#$%#$$&^%*%$#$$%#$^$^&$#$$@#%$%&%^%$^&>*')
+    print(f'      {colors.FG_RST}Created by: {colors.B_CYAN}{__author__}')
+    print(f'      {colors.FG_RST}Version: {colors.B_CYAN}{__version__}')
+    print(f'{colors.RST}')
+
+# Terminal colors
+class colors:
+    # Text effects
+    RST = '\033[0m'
+    FG_RST = '\033[39m'
+    BG_RST = '\033[49m'
+    BOLD = '\033[1m'
+
+    # Regular colors
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    YELLOW = '\033[33m'
+    BROWN = '\033[38;2;220;120;50m'
+
+    # Backgrounds
+    BG_RED = '\033[41m'
+    BG_YELLOW = '\033[43m'
+    BG_CYAN = '\033[46m'
+
+    # Bright colors
+    B_RED = '\033[91m'
+    B_GREEN = '\033[92m'
+    B_YELLOW = '\033[93m'
+    B_CYAN = '\033[96m'
+
+"""
+Print colored output to the screen.
+Available types (Int):
+1. WARNING
+2. INFO
+3. ERROR
+4. CONFIRM
+5. DANGER
+6. SUCCESS  (Small)
+7. FAILED   (Small)
+"""
+def write(text, type):
+    if type == 1:
+        print(f'{colors.BOLD}{colors.BG_YELLOW}{colors.BLACK}[WARNING]{colors.RST} {text}')
+    elif type == 2:
+        print(f'{colors.BOLD}[{colors.YELLOW}INFO{colors.FG_RST}]{colors.RST} {text}')
+    elif type == 3:
+        print(f'{colors.BOLD}[{colors.B_RED}ERROR{colors.FG_RST}]{colors.RST}{colors.B_RED} {text}{colors.RST}')
+    elif type == 4:
+        ans = input(f'{colors.BOLD}{colors.BG_CYAN}[CONFIRM]{colors.RST} {text}')
+        return ans
+    elif type == 5:
+        print(f'{colors.BOLD}{colors.BG_RED}[DANGER]{colors.BG_RST}{colors.B_RED} {text}{colors.RST}')
+    elif type == 6:
+        print(f'{colors.BOLD}[{colors.B_GREEN}+{colors.FG_RST}]{colors.RST}{colors.B_GREEN} {text}{colors.RST}')
+    elif type == 7:
+        print(f'{colors.BOLD}[{colors.B_RED}-{colors.FG_RST}]{colors.RST}{colors.B_RED} {text}{colors.RST}')
+
 def deleteFiles():
-    print("[WARNING] Deleting log files...")
+    write('Deleting log files...', 1)
     for log in logFiles:
         try:
             os.remove(log)
-            print("[+] Removed: " + log)
+            write(f'Removed: {log}', 6)
         except:
-            print("[-] Not found: " + log)
+            write(f'Not found: {log}', 7)
 
 def deleteLogDirs():
-    print("[INFO] Looking for files in common directories...")
+    write('Looking for files in common directories...', 2)
     for folder in logFileDirs:
         try:
             for file in os.listdir(folder):
@@ -159,14 +214,14 @@ def deleteLogDirs():
                     elif os.path.isdir(file_path):
                         shutil.rmtree(filepath)
                 except:
-                    print("[-] Failed to delete: " + file_path)
+                    write(f'Failed to delete: {file_path}', 7)
         except:
-            print("[-] Folder not found: " + folder)
+            write(f'Folder not found: {folder}', 7) 
 
 def confirm():
-    ans = input("[CONFIRM] Are you sure you want to delete the files? Y/n: ")
+    ans = write('Are you sure you want to delete the files? Y/n: ', 4)
     if (str.lower(ans) == "n"):
-        print("[INFO] Exiting...")
+        write('Exiting...', 2)
         return False
     return True
 
@@ -179,24 +234,26 @@ def main(argv):
         argStack = [*arg]
 
         if (arg[0] != "-"):
-            print("[ERROR] The following argument " + arg + " was not understood!")
+            write(f'The following argument "{arg}" was not understood!', 3)
             return
         
         for letter in argStack[1:]:
             if (letter == "A"):
                 if (uid == 0):
+                    write('Superuser privileges detected!', 5)
+                    write('This will delete all of the log and history files!', 1)
                     if (not confirm()): return
 
-                    print("[INFO] Superuser privileges detected!")
-                    print("[WARNING] This will delete all of the log and history files!")
-
-                    deleteFiles()
-                    deleteLogDirs()
+                    #deleteFiles()
+                    #deleteLogDirs()
             elif(letter == "h"):
-                print("Help Menu")
+                print('Help Menu')
             else:
-                print("[ERROR] The following argument " + arg + " was not understood!")
-        
+                write(f'The following argument {arg} was not understood!', 3)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        print()
+        write('Exiting...', 2)
